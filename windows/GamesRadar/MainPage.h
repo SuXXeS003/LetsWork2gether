d6a072495e019612fa22c90146087e5aa5a66733
Swarly <27783184+Swarly1803@users.noreply.github.com>
2022-07-01 23:34:32 +0200

    setup react native for windows build

---
+++ windows/GamesRadar/MainPage.h
@@ -0,0 +1,19 @@
+﻿#pragma once
+#include "MainPage.g.h"
+#include <winrt/Microsoft.ReactNative.h>
+
+namespace winrt::GamesRadar::implementation
+{
+    struct MainPage : MainPageT<MainPage>
+    {
+        MainPage();
+    };
+}
+
+namespace winrt::GamesRadar::factory_implementation
+{
+    struct MainPage : MainPageT<MainPage, implementation::MainPage>
+    {
+    };
+}
+
